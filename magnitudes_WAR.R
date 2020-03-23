62750dd2b2d96cb9db571c98438283b1ae37dc6b
Victor <mitchell.victorj@gmail.com>
2020-03-23 15:37:12 +0100

    15 37 name correction path introduction

---
+++ magnitudes_WAR.R
@@ -0,0 +1,57 @@
+
+library(ggplot2)
+library(ggthemes)
+
+####
+X_t <- t(as.matrix(data_1$std_Difference))
+
+
+plot(B1_t[11,], type = "l")
+for (i in 1:10){
+  lines(B1_t[i,])
+}
+lines(B1_final, col = "red")
+
+N <- 144 
+
+# grw <- rnorm(N, B1, 5)
+# lines(grw, col = "blue")
+
+
+####
+#plot(w_new, type = "l")
+
+# ggplot(data=B1_t, aes(x = X, y = B1_final)) +
+#   geom_line() +
+#   geom_line(aes(x = X, y = upper), color="dark gray", linetype = "dotdash") +
+#   geom_line(aes(x = X, y = lower), color="dark gray", linetype = "dotdash") +
+#   geom_hline(yintercept = 0, linetype="dashed", color = "dark gray") +
+#   ylab("Beta") +
+#   ggtitle(title) +
+#   theme_tufte()
+
+#gbpcpi_magn <- read.csv("~/R tests/finance related projects/gbpcpi_magn.csv")
+#final_path <- read.csv("~/R tests/finance related projects/gbpcpi_path.csv")
+B1_final <- final_path$B1_final
+Time <- seq(0,135)
+dat <- data.frame(t(rbind(Time, gbpcpi_magn)))[-1,]
+dat <- data.frame(cbind(B1_final, dat))
+c("Time","one","two","three","four","five","six","seven","eight","nine","ten","eleven") <- colnames(dat)
+ggplot(data=dat, aes(x = X1)) +
+  geom_line(aes(y = X2), color = "steelblue", linetype = "twodash") +
+  geom_line(aes(y = X3)) +
+  geom_line(aes(y = X4)) +
+  geom_line(aes(y = X5)) +
+  geom_line(aes(y = X6)) +
+  geom_line(aes(y = X7)) +
+  geom_line(aes(y = X8)) +
+  geom_line(aes(y = X9)) +
+  geom_line(aes(y = X10)) +
+  geom_line(aes(y = X11)) +
+  geom_line(aes(y = X12)) +
+  geom_line(aes(y = B1_final), color = "green") 
+  
+plotwar <- function(betapaths, title){
+  
+  
+}
