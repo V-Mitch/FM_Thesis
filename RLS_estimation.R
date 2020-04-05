0c3732a7d5647a462eac00939f02476bd16bb1d1
Victor <mitchell.victorj@gmail.com>
2020-04-05 15:51:46 +0200

    15 51 rls compare

--- RLS_estimation.R
+++ RLS_estimation.R
@@ -7,7 +7,7 @@ T <- nrow(data_1)
 beta_hat <- rep(0,T)
 g_t <- rep(0,T)
 p_t <- rep(0,T)
-p_0 <- 0.1
+p_0 <- 0.23
 S_t <- data_1$std_Difference 
 R_t <- data_1$m5
 
@@ -21,4 +21,10 @@ p_t[t] <- p_t[t-1] - g_t[t] * S_t[t] * p_t[t-1]
 beta_hat[t] <- beta_hat[t-1] + g_t[t] * (R_t[t] - S_t[t] * beta_hat[t-1])
 }
 
-plot(beta_hat, type = "l")
+par(mfrow=c(1,2))
+plot(beta_hat, type = "l", ylim = c(-13,2))
+lines(rep(mod1$coefficients[2],T), lty = "dotdash")
+
+rls_mod <- RLS(data_1$m5, data_1$std_Difference, ist = 30)
+plot(rls_mod$beta, type = "l", ylim = c(-13,2))
+lines(rep(mod1$coefficients[2],T), lty = "dotdash")
