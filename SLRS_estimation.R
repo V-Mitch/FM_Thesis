444a4f1361af917d14f15d246baf1a883c9aa4e3
Victor <mitchell.victorj@gmail.com>
2020-04-10 09:19:33 +0200

    SLRS variance correction and bounds creation

--- SLRS_estimation.R
+++ SLRS_estimation.R
@@ -10,16 +10,19 @@ S_t <- t(as.matrix(data_1$std_Difference))
 #S_t <- rbind(data_1$std_Difference, data_2$std_Difference, data_3$std_Difference)
 
 Y_t <- t(as.matrix(data_1$m5))
-
+icept <- rep(1,T)
+S_t <- rbind(icept, S_t) 
 
 k <- dim(S_t)[1]
 T <- nrow(data_1)
 beta_hat <- matrix(0,nrow = k, ncol = T)
-beta_hat[1] <- 12
+upper <- matrix(0,nrow = k, ncol = T)
+lower <- matrix(0,nrow = k, ncol = T)
+beta_hat[,1] <- c(-0.4458, 12.6814)
 g_t <- matrix(0,nrow = k, ncol = T)
 p_t <- matrix(seq(0,0),nrow = k, ncol = k) # p_t matrix will continuously be replaced
 p_0 <- matrix(seq(0,0),nrow = k, ncol = k)
-diag(p_0) <- 0.23
+diag(p_0) <- c(1.7294,1.7288)
 
 sigma <- sqrt(1/(T-dim(S_t)[1] -1) * sum(mod1$residuals^2))
 
@@ -27,20 +30,24 @@ sigma <- sqrt(1/(T-dim(S_t)[1] -1) * sum(mod1$residuals^2))
 R_t <- data_1$m5
 
 # Initialization
-g_t[,1] <- p_0 %*% S_t[,1] %*% (sigma + t(S_t[,1]) %*% p_0 %*% S_t[,1]) ^ -1
+g_t[,1] <- p_0 %*% S_t[,1] %*% (sigma^2 + t(S_t[,1]) %*% p_0 %*% S_t[,1]) ^ -1
 p_t <- p_0 - g_t[,1] %*% t(S_t[,1]) %*% p_0
 
 # nrow(data_1
 for(t in 2:nrow(data_1)){
-  g_t[,t] <- p_t %*% S_t[,t] %*% ( sigma + t(S_t[,t]) %*% p_t %*% S_t[,t]) ^ -1
+  g_t[,t] <- p_t %*% S_t[,t] %*% ( sigma^2 + t(S_t[,t]) %*% p_t %*% S_t[,t]) ^ -1
   p_t <- p_t - g_t[,t] %*% t(S_t[,t]) %*% p_t
   beta_hat[,t] <- beta_hat[,t-1] + g_t[,t] %*% (R_t[t] - S_t[,t] %*% beta_hat[,t-1])
-  upper[,t] <- sqrt(1/T*(dim(X_t[1])-1)*mod1$residuals^2)
+  #upper[,t] <- sqrt(1/T*(dim(X_t[1])-1)*mod1$residuals^2)
+  upper[,t] <- beta_hat[,t] + 1.96 * sqrt(diag(p_t)/sqrt(t))
+  lower[,t] <- beta_hat[,t] - 1.96 * sqrt(diag(p_t)/sqrt(t))
 }
 
 #par(mfrow=c(1,2))
-plot(beta_hat[1,] ~ as.Date(data_1$Date), type = "l")
+plot(beta_hat[2,] ~ as.Date(data_1$Date), type = "l")
 lines(rep(mod1$coefficients[2],T) ~ as.Date(data_1$Date), lty = "dotdash")
+lines(upper[2,]~ as.Date(data_1$Date), type = "l", lty = 3)
+lines(lower[2,]~ as.Date(data_1$Date), type = "l", lty = 3)
 
 
 # rls_mod <- RLS(data_1$m5, data_1$std_Difference, ist = 30)
